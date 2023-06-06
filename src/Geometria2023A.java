8422be31d1e153b4bf7d32f5d85999b221b9d913
Cristhian Panora <cristhian.panora@gmail.com>
2023-06-06 07:37:25 -0500

    se añadio la longitud de los lados

--- src/Geometria2023A.java
+++ src/Geometria2023A.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class Geometria2023A {
     public static void main(String[] args) {
 
     }
--- src/figuras2D.java
+++ src/figuras2D.java
@@ -1,2 +1,30 @@
 public class figuras2D {
+
+    int n_lados; //numero de lados
+    float lados; //longitud del lado
+
+    //Setter y Getter
+    public int getN_lados() {
+        return n_lados;
+    }
+
+    public void setN_lados(int n_lados) {
+        this.n_lados = n_lados;
+    }
+
+    //Constructor
+
+    public figuras2D(int n_lados, float lados) {
+        this.n_lados = n_lados;
+        this.lados = lados;
+    }
+
+
+    public float areaCuadrado(){
+
+    };
+    private float perimetroCuadrado(){
+
+    };
+
 }
